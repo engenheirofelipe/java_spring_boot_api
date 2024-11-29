182cea96fabac8abf87fb32376520e9b858763ac
Felipe <felipeferreira.tech@outlook.com>
2024-11-28 23:44:38 -0300

    Atualizando

--- principal/Principal.java
+++ principal/Principal.java
@@ -1,6 +1,7 @@
 package br.com.fiap.consumoapi.principal;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -43,15 +44,20 @@ public class Principal {
 		}
 		temporadas.forEach(System.out::println);
 		
-		for(int i = 0; i< dados.totalTemporadas(); i++) {
-			List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
-			for(int j = 0; j < episodiosTemporada.size(); j++) {
-				System.out.println(episodiosTemporada.get(j).titulo());
-			}
-		}
+//		for(int i = 0; i< dados.totalTemporadas(); i++) {
+//			List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
+//			for(int j = 0; j < episodiosTemporada.size(); j++) {
+//				System.out.println(episodiosTemporada.get(j).titulo());
+//			}
+//		}
 		
-//		temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+//		/*Lambda concept*/
+		temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
 		
+		/*Streams*/
+		List<DadosEpisodio> dadosEpisodio = temporadas.stream()
+				.flatMap(t -> t.episodios().stream())
+				.toList()
 	}
 
 
